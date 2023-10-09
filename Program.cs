18f9c2e69f20e316d611fdce5355dd8a274c4dd1
marcelldac <dacmarcell@gmail.com>
2023-10-09 12:53:54 -0300

    feat: add product structure

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace myApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Clear();
+      Product mouse = new(Guid.NewGuid(), "Mouse Gamer RGB 3000", 299.90, EDiscount.TenPercent);
+      string text = $"The product price is: {mouse.Price} on {(int)mouse.Discount}% promotion";
+      Console.WriteLine(text);
+    }
+
+
+  }
+  struct Product
+  {
+    public Product(Guid id, string name, double price, EDiscount discount)
+    {
+      Id = id;
+      Name = name;
+      Price = price;
+      Discount = discount;
+    }
+
+    public Guid Id;
+    public string Name;
+    public double Price;
+    public EDiscount Discount;
+  }
+
+  enum EDiscount
+  {
+    TenPercent = 10,
+    TwentyPercent = 20
+  }
+}
